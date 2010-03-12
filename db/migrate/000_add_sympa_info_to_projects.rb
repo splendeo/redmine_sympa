d3e7af73280684b6573e3e22534921ea440a993e
egarcia <egarcia@splendeo.es>
2010-03-12 10:35:10 +0100

    corrected migration version number

--- db/migrate/000_add_sympa_info_to_projects.rb
+++
@@ -1,9 +0,0 @@
-class AddSympaInfoToProjects < ActiveRecord::Migration
-  def self.up
-    add_column :projects, :sympa_info, :text
-  end
-
-  def self.down
-    remove_column :projects, :sympa_info
-  end
-end
