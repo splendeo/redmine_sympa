2f63c46b218cfbc190e77d65fa1dd54e702fd581
Enrique García <kikito@gmail.com>
2011-02-09 17:26:09 +0100

    updated to redmine 1.1 - project now activates modules correctly

--- lib/redmine_sympa/patches/project_patch.rb
+++ lib/redmine_sympa/patches/project_patch.rb
@@ -6,13 +6,6 @@ module RedmineSympa
         base.extend(ClassMethods)
         base.class_eval do
           unloadable # Send unloadable so it will not be unloaded in development
-
-          # This replaces the existing version of enabled_module_names with a new one
-          # It is needed because we need the "destroy" callbacks to be fired,
-          # and only on the erased modules (not all of them - the default implementation
-          # starts by wiping them out - v0.8)
-          alias_method :enabled_module_names=, :sympa_enabled_module_names=
-
         end
       end
 
@@ -22,17 +15,6 @@ module RedmineSympa
       module InstanceMethods
         def has_sympa_mailing_list?
           return self.module_enabled?(:sympa_mailing_list)
-        end
-
-        # Redefine enabled_module_names so it invokes mod.destroy on disconnected modules
-        def sympa_enabled_module_names=(module_names)
-          module_names = [] unless module_names and module_names.is_a?(Array)
-          module_names = module_names.collect(&:to_s)
-          # remove disabled modules
-          enabled_modules.each {|mod| mod.destroy unless module_names.include?(mod.name)}
-          
-          # detect the modules that are new, and create those only
-          module_names.reject {|name| module_enabled?(name)}.each {|name| enabled_modules << EnabledModule.new(:name => name) }
         end
         
         def sympa_mailing_list_address
