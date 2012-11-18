8e7925d3e890d3a014b1f1632316100c0684da7c
fjuan <fjuan@splendeo.es>
2012-11-18 09:32:11 +0100

    Add sympa_info as safe_attributes

--- lib/redmine_sympa/patches/project_patch.rb
+++ lib/redmine_sympa/patches/project_patch.rb
@@ -5,6 +5,8 @@ module RedmineSympa
         base.send(:include, InstanceMethods)
         base.class_eval do
           unloadable # Send unloadable so it will not be unloaded in development
+          
+          safe_attributes 'sympa_info'
         end
       end
 
